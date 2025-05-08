2e1e70eb8fbedf75b7d42e04990874c157152817
necatiboga <boganecati21@gmail.com>
2025-05-08 11:22:36 +0300

    add  query failer error exception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -3,25 +3,47 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
+import { QueryFailedError } from 'typeorm';
 
-@Catch(HttpException)
+@Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const status = exception.getStatus();
-    const exceptionResponse = exception.getResponse();
 
-    response.status(status).json({
-      statusCode: status,
-      message: 'error',
-      data: null,
-      error:
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message = 'Internal server error';
+    let error = null;
+
+    /** Process for QueryFailedError */
+    if (exception instanceof QueryFailedError) {
+      if (exception.message.includes('duplicate key')) {
+        if (exception.message.includes('Users_username_key')) {
+          message = 'Username already exists';
+        } else if (exception.message.includes('Users_email_key')) {
+          message = 'Email already exists';
+        } else {
+          message = 'Duplicate key error';
+        }
+      }
+    } else if (exception instanceof HttpException) {
+      /** Process for HttpException */
+      status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+      message = exception.message;
+      error =
         typeof exceptionResponse === 'string'
           ? exceptionResponse
-          : (exceptionResponse as any).message || exception.message,
+          : (exceptionResponse as any).message;
+    }
+    response.status(status).json({
+      statusCode: status,
+      message: message,
+      data: null,
+      error: error || message,
     });
   }
 }
